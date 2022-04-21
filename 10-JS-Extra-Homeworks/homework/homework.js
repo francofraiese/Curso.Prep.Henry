36ab51e04bf983e2e13921db6faac85de23f7fe5
Franco Fraiese <fraancofraiese@gmail.com>
2022-04-20 21:09:30 -0300

    A

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  Arreglo = [];
+  for (i = 0; i < objeto.length; i++){
+  Arreglo.push(objeto[i] + ': ' + i);
+  return Arreglo;
+}
 }
 
 
